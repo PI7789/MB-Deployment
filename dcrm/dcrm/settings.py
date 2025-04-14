79945fffb72aacf87171fcf442f2a6c92fee4dbd
PI7789 <160234244@student.rnngroup.ac.uk>
2025-04-14 14:06:09 +0100

    twelfth commit

--- dcrm/dcrm/settings.py
+++ dcrm/dcrm/settings.py
@@ -125,11 +125,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'  # URL prefix for serving static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # This points to the root-level 'static' folder
+    BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
